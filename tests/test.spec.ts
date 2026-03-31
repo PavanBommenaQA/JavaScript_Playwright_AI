54b296a32a1b21b680231301deb479492b41dcd8
Pavan Bommena <pavan.bommena95@gmail.com>
2026-03-31 15:19:45 +0530

    playwright

---
+++ tests/test.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test.only('nimble login smoke test', async ({ page }) => {
+
+    await page.goto('https://qa.nimblestaffing.net/');
+
+    await page.locator('#tbUserName').fill('pavan.bommena@nimbleaccounting.com');
+    await page.locator('#tbPassword').fill('Nimble#2025');
+    await page.locator('#bLoginButton').click();
+
+    const mainFrame = page.frameLocator("//iframe[@id='ifNimble']");
+    await expect(mainFrame.locator(".loader_inner_wrap")).toBeHidden();
+
+    // Handle dialog BEFORE click
+    page.once('dialog', async dialog => {
+        await dialog.accept();
+    });
+    const dropdown = mainFrame.locator('select[name="company"]');
+    await dropdown.waitFor();
+    await dropdown.click();
+    await dropdown.selectOption({ label: 'BMW AG' });
+    await page.keyboard.press('Tab');
+    // Print selected value
+    const selected = await dropdown.locator('option:checked').textContent();
+    console.log('Selected:', selected);
+});
